f5b264eeaebd25ab72186eb1e12006e143924985
AndrewsGilbert <tandrewsgilbert@gmail.com>
2021-07-22 11:16:39 +0530

    cookie method added

--- work.ts
+++ work.ts
@@ -1,9 +1,12 @@
+
 import express from 'express'
 import fs from 'fs'
 import bodyParser from 'body-parser'
 import alert from 'alert'
 import path from 'path'
 const app = express()
+import cookieParser from 'cookie-parser'
+app.use(cookieParser())
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -18,39 +21,70 @@ const content: string = fs.readFileSync('det.json', 'utf8')
 const contentJson: content = JSON.parse(content)
 const dataId:number = contentJson.id
 const dataCode:number = contentJson.pass
+let auth:string = 'no'
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/home.HTML'))
-})
 
-app.get('/beforelogin', function (req, res) {
-  alert('Please login to view profile')
-  res.sendFile(path.join(__dirname, '/home.HTML'))
-})
-
-app.post('/index', function (req, res) {
-
-    const id:number = Number(req.body.id)
-    const code:number = Number(req.body.code)
-
-    if( dataId === id && dataCode === code ){
-        res.sendFile(path.join(__dirname, '/viewprofile.HTML'))
+    if(Object.keys(req.cookies).length !== 0){
+        res.redirect('http://localhost:5001/index')
     }
-     else { 
-        alert('Please provide the corrct user id or password')
+    else{
         res.sendFile(path.join(__dirname, '/home.HTML'))
     }
 })
 
-app.get('/afterlogin', function (req, res) {
-  res.sendFile(path.join(__dirname, '/profile.HTML'))
+app.get('/index', function (req, res) {
+
+    if(Object.keys(req.cookies).length !== 0){
+        res.sendFile(path.join(__dirname, '/viewprofile.HTML'))
+    }
+    else{
+        res.sendFile(path.join(__dirname, '/home.HTML'))
+    }
 })
+
+app.post('/index', function (req, res) {
+
+    if(Object.keys(req.cookies).length !== 0){
+        res.sendFile(path.join(__dirname, '/viewprofile.HTML'))
+    }
+
+    else if (auth === 'yes' ) {
+        auth = 'no'
+        res.sendFile(path.join(__dirname, '/home.HTML'))
+    }
+    else
+    {
+        if( dataId === Number(req.body.id) && dataCode === Number(req.body.code) ){
+            res.cookie('data',{"Id":dataId,"pass":dataCode})
+            console.log(dataId,dataCode)
+            console.log(3)
+            auth = 'yes'
+            res.sendFile(path.join(__dirname, '/viewprofile.HTML'))
+        }
+        else {
+            alert('Please provide the corrct user id or password')
+            res.sendFile(path.join(__dirname, '/home.HTML'))
+        }
+    }
+})
+
+app.get('/index/detail', function (req, res) {
+    if(Object.keys(req.cookies).length !== 0){
+        res.sendFile(path.join(__dirname, '/profile.HTML'))
+    }
+    else{
+        res.sendFile(path.join(__dirname, '/home.HTML'))
+    }
+  })
 
 app.get('/logout', function (req, res) {
-  alert('Successfully logged out')
-  res.sendFile(path.join(__dirname, '/home.HTML'))
-})
+    res.clearCookie('data')
+    alert('Successfully logged out')
+    res.redirect('http://localhost:5001')
+  })
 
-app.listen(5000, function () {
+app.listen(5001, function () {
   console.log('Node server is running..')
 })
+
